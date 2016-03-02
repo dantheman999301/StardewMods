f5cbbe18f2ce9703644072fd3df9ac720dc732be
dantheman999 <d.edwards.15.de@gmail.com>
2016-03-02 18:56:03 +0000

    Fixed not growing at correct times, slightly simplified

--- LessVariableGrass/VariableGrass.cs
+++ LessVariableGrass/VariableGrass.cs
@@ -19,7 +19,7 @@ namespace LessVariableGrass
 
         public override string Version
         {
-            get { return "1.0"; }
+            get { return "1.1"; }
         }
 
         public override string Description
@@ -32,25 +32,25 @@ namespace LessVariableGrass
             Events.CurrentLocationChanged += Events_CurrentLocationChanged;
         }
 
-        private int day = -1;
+        private int day;
         private int min = 0;
         private int max = 1;
         Random rnd = new Random();
 
         void Events_CurrentLocationChanged(GameLocation Location)
         {
-
             Console.WriteLine("Location changed :: " + Location.Name);
+
             if (!SGame.hasLoadedGame)
                 return;
+
             Console.WriteLine("Current day :: " + day);
             Console.WriteLine("Current game day :: " + SGame.dayOfMonth);
-            if (day == -1 || day == SGame.dayOfMonth)
-            {
-                day = SGame.dayOfMonth;
-                return;
-            }
 
+            if (day == SGame.dayOfMonth)
+                return;
+
+            day = SGame.dayOfMonth;
             Console.WriteLine("Getting farm...");
             Farm farm = SGame.getLocationFromName("Farm") as Farm;
             Console.WriteLine("Got farm... GROW!");
